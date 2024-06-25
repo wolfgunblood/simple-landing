df75bd611cdd77c52381f8171111cfeeea0ca694
wolfgunblood <jalajdorai1@gmail.com>
2024-06-25 16:22:39 +0530

    done with landing page

--- components/Shiny.tsx
+++ components/Shiny.tsx
@@ -1,20 +1,21 @@
 import { cn } from "@/lib/utils";
-import AnimatedShinyText from "@/components/magicui/animated-shiny-text";
-import { ArrowRightIcon } from "@radix-ui/react-icons";
+import AnimatedGradientText from "@/components/magicui/animated-gradient-text";
+import { ChevronRight } from "lucide-react";
 
-export async function AnimatedShiny() {
+export async function AnimatedGradient() {
   return (
-    <div className="z-10 flex min-h-fit items-center justify-center my-5">
-      <div
-        className={cn(
-          "group rounded-full border border-black/5 bg-neutral-100 text-base text-white transition-all ease-in hover:cursor-pointer hover:bg-neutral-200 dark:border-white/5 dark:bg-neutral-900 dark:hover:bg-neutral-800",
-        )}
-      >
-        <AnimatedShinyText className="inline-flex items-center justify-center px-4 py-1 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400">
-          <span>✨ Introducing Open SB</span>
-          <ArrowRightIcon className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
-        </AnimatedShinyText>
-      </div>
+    <div className="z-10 flex min-h-[16rem] items-center justify-center">
+      <AnimatedGradientText>
+        🎉 <hr className="mx-2 h-4 w-[1px] shrink-0 bg-gray-300" />{" "}
+        <span
+          className={cn(
+            `inline animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent`,
+          )}
+        >
+          Introducing Simple
+        </span>
+        <ChevronRight className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
+      </AnimatedGradientText>
     </div>
   );
 }

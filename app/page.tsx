2dd1e4bb5951b3ee67ffce4ed23df86d5d84ea21
wolfgunblood <jalajdorai1@gmail.com>
2024-06-25 15:54:32 +0530

    done with landing page

---
+++ app/page.tsx
@@ -0,0 +1,27 @@
+import { Bento } from "@/components/Bento";
+import CTA from "@/components/cta";
+import { FAQ } from "@/components/FAQs";
+import Features from "@/components/Features";
+import Footer from "@/components/footer";
+import Hero from "@/components/Hero";
+import HeroImage from "@/components/HeroImage";
+import Testimonial from "@/components/Marquee";
+import Navbar from "@/components/Navbar";
+import { Pricing } from "@/components/Pricing";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+      <>
+      <Navbar />
+      <Hero />
+      {/* <Features /> */}
+      <HeroImage />
+      <Pricing />
+      <FAQ />
+      <Testimonial />
+      <CTA />
+      <Footer />
+      </>
+  );
+}

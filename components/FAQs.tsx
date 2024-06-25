78f1b503138b824431bd247c44567ac1879e04e6
wolfgunblood <jalajdorai1@gmail.com>
2024-06-25 17:56:39 +0530

    done with faqs

--- components/FAQs.tsx
+++ components/FAQs.tsx
@@ -27,31 +27,13 @@ const FAQList: FAQProps[] = [
       answer: "Yes, the template is built with responsive design principles in mind, ensuring that your website will look great on all devices, from phones to desktops.",
       value: "item-3",
     },
+
     {
-      question: "Are there any tutorials available for this template?",
-      answer: "Yes, we provide comprehensive documentation and step-by-step tutorials to help you get started and make the most of the template’s features.",
-      value: "item-4",
-    },
-    {
-      question: "Can I use this template for multiple projects?",
-      answer: "Yes, once you download the template, you are free to use it for multiple projects, making it an excellent choice for developers and agencies.",
-      value: "item-5",
-    },
-    {
-      question: "Does this project utilize Magic UI components?",
-      answer: "Yes, our template integrates Magic UI components to provide enhanced UI capabilities and a seamless user experience.",
-      value: "item-6",
-    },
-    {
-      question: "Is MongoDB used for data management in this template?",
-      answer: "Yes, our template is configured to work seamlessly with MongoDB, allowing for efficient data management and scalability.",
+      question: "What database management solutions are supported?",
+      answer: "Our template is configured to work seamlessly with MongoDB, allowing for efficient data management and scalability.",
       value: "item-7",
     },
-    {
-      question: "Are you planning to integrate Supabase in the future?",
-      answer: "Not yet, but we are considering integrating Supabase to enhance our backend capabilities and provide additional features like real-time data.",
-      value: "item-8",
-    },
+   
     {
       question: "What authentication solutions are supported?",
       answer: "Our template supports stack-auth open source authentication platform",

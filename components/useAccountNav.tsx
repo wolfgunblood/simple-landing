a34dcee5aedc12b43c69268eb82c4373939122ed
wolfgunblood <jalajdorai1@gmail.com>
2024-06-26 13:20:51 +0530

    image issue

--- components/useAccountNav.tsx
+++ components/useAccountNav.tsx
@@ -22,30 +22,42 @@ export function UserAccountNav() {
   const app = useStackApp();
   const user = app.useUser();
 
+  // Styling for the image to make it round
+  const imageStyle = {
+    width: "40px", // Set the width of the image
+    height: "40px", // Set the height of the image
+    borderRadius: "50%", // Make the image round
+    objectFit: "cover", // Cover the area without distorting the image
+  };
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         {/* <Button variant="outline"> */}
-        <Avatar>
-          <AvatarImage src={user?.profileImageUrl} />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
+        <img
+          src={user?.profileImageUrl}
+          alt="User Avatar"
+          style={{
+            width: "40px",
+            height: "40px",
+            borderRadius: "50%",
+            objectFit: "cover",
+          }}
+        />
+
         {/* </Button> */}
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56">
         <DropdownMenuLabel>My Account</DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuGroup>
-            <DropdownMenuItem>
+          <DropdownMenuItem>
             <p>Welcome, {user?.displayName}</p>
-
-            </DropdownMenuItem>
-            <DropdownMenuItem>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
             <p>Your e-mail: {user?.primaryEmail}</p>
-
-            </DropdownMenuItem>
-           
-          </DropdownMenuGroup>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
         <DropdownMenuSeparator />
         <DropdownMenuItem onClick={() => user?.signOut()}>
           <LogOut className="mr-2 h-4 w-4" />

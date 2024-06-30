4dc9ba145747c93da654dc08ab9a1d44bb39c14a
wolfgunblood <jalajdorai1@gmail.com>
2024-06-30 20:48:02 +0530

    uppdated opengraph image

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,10 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Next.js Saas Kit",
-  description: "A Next.js starter kit with Stack  auth.",
+  description: "A Next.js starter kit",
   openGraph: {
     title:  "Next.js Saas Kit",
-    description: "A Next.js starter kit with Stack auth.",
+    description: "A Next.js starter kit ",
     url: "https://saaskit.one/",
     siteName: `Saaskit`,
     locale: "en_US",
@@ -21,10 +21,10 @@ export const metadata: Metadata = {
     images: `https://utfs.io/f/6065a913-376e-43a3-a59d-519c358e737f-8pkbvt.png`
   },
   twitter: {
-    card: "summary_large_image",
+    // card: "summary_large_image",
     title: "Next.js Saas Kit",
-    description: "A Next.js starter kit with Stack auth.",
-    images: "https://utfs.io/f/6065a913-376e-43a3-a59d-519c358e737f-8pkbvt.png",
+    description: "A Next.js starter kit",
+    // images: "https://utfs.io/f/6065a913-376e-43a3-a59d-519c358e737f-8pkbvt.png",
     site: "@jalajdu" // Replace with your actual Twitter handle
   }
 };

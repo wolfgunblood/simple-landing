915cc5c9e64597038e60383673d237809667997d
wolfgunblood <jalajdorai1@gmail.com>
2024-07-12 13:05:11 +0530

    added evhunt banner

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,31 +5,31 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Next.js Saas Kit",
+  title: "Next.js SaaS Kit",
   description: "A Next.js starter kit",
   openGraph: {
-    title: "Next.js Saas Kit",
-    description: "A Next.js starter kit ",
+    title: "Next.js SaaS Kit",
+    description: "A Next.js starter kit",
     url: "https://saaskit.one/",
-    siteName: `Saaskit`,
+    siteName: "Saaskit",
     locale: "en_US",
     type: "website",
-    // images: "/landing-page.png",
-    images: `https://utfs.io/f/6065a913-376e-43a3-a59d-519c358e737f-8pkbvt.png`
+    images: "https://utfs.io/f/6065a913-376e-43a3-a59d-519c358e737f-8pkbvt.png",
   },
   twitter: {
     card: "summary_large_image",
-    title: "Next.js Saas Kit",
+    title: "Next.js SaaS Kit",
     description: "A Next.js starter kit",
-    // images: "/landing-page.png",
     images: "https://utfs.io/f/6065a913-376e-43a3-a59d-519c358e737f-8pkbvt.png",
-    site: "@jalajdu" // Replace with your actual Twitter handle
-  }
+    site: "@jalajdu", // Replace with your actual Twitter handle
+  },
 };
+
 export const runtime = "edge";
 
 export default function RootLayout({
@@ -39,19 +39,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <link rel="icon" type="image/svg+xml" href="/logo.svg" />
+        <link rel="icon" type="image/svg+xml" href="/logo.svg" />
 
-      <body className={inter.className}><StackProvider app={stackServerApp}><StackTheme>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-          <Toaster />
-        </ThemeProvider>
-      </StackTheme></StackProvider></body>
+      <body className={inter.className}>
+        <StackProvider app={stackServerApp}>
+          <StackTheme>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="dark"
+              enableSystem
+              disableTransitionOnChange
+            >
+              <script
+                defer
+                data-url="https://devhunt.org/tool/saaskit-"
+                src="https://cdn.jsdelivr.net/gh/sidiDev/devhunt-banner/indexV0.js"
+              />
+              {children}
+              <Toaster />
+            </ThemeProvider>
+          </StackTheme>
+        </StackProvider>
+      </body>
     </html>
   );
 }

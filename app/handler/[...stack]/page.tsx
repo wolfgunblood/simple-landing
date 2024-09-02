120be49a22da9725ae4cec5801535806e720b9bf
wolfgunblood <jalajdorai1@gmail.com>
2024-09-02 15:50:49 +0530

    delete all files

--- app/handler/[...stack]/page.tsx
+++
@@ -1,6 +0,0 @@
-import { StackHandler } from "@stackframe/stack";
-import { stackServerApp } from "../../../stack";
-export const runtime = 'edge'
-export default function Handler(props: any) {
-  return <StackHandler fullPage app={stackServerApp} {...props} />;
-}
